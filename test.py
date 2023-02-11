01aa105b063d8d0388eda1e84e0be42c5e3c8e22
Rohith <email2rohithraj@gmail.com>
2023-02-11 13:28:26 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+#ask for input from user and store it in a variable
+name = input("What is your name? ")
+#ask for input from user and store it in a variable
+age = input("How old are you? ")
+#ask for input from user and store it in a variable
+city = input("Where do you live? ")
+#ask for input from user and store it in a variable
+language = input("What language do you speak? ")
+#print the variables
+print("So, you're %r, %r old, live in %r and you speak %r." % (name, age, city, language))
+
